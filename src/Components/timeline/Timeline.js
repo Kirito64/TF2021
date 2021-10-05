e9d89f709da113b59ccadfb462916f199ad6e0e5
Ayush Pratap Singh <ayushpratap344@gmail.com>
2021-10-05 22:13:12 +0530

    Initiating Timeline folder structure

---
+++ src/Components/timeline/Timeline.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// importing svgs
+// import { ReactComponent as Line } from "./assets/line.svg";
+// import { ReactComponent as InnerCircle } from "./assets/ellipse1.svg";
+// import { ReactComponent as OuterCircle } from "./assets/ellipse2.svg";
+
+const Timeline = () => {
+    return (
+       <>
+            <h1>Timeline</h1>
+       </>
+    )
+}
+
+export default Timeline
