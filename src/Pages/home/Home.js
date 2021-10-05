8d73ddf405d3e2b28ba36f24994992a3301a82ca
Jatin Chhhangani <jatinchhangani@gmail.com>
2021-10-05 20:40:39 +0530

    to fix: css InfoCard:

--- src/Pages/home/Home.js
+++ src/Pages/home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import InfoCard from "../../Components/infoCard/InfoCard";
 const Home = () => {
     return (
         <div>
             <h1>Home Page</h1>
+            <InfoCard heading = "Hackathon" content = "With the team size of 1-4 hackers will be solving problem statements..." deadline = "asdfas" url = "#"></InfoCard>
         </div>
     )
 }
