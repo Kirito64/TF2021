b5601d2f04cf7925c117e797a8563486bf06ff61
devu <80763211+Devanshu236@users.noreply.github.com>
2021-10-06 16:50:35 +0530

    layout is ready

--- src/Components/eventCarousel/Slider.js
+++ src/Components/eventCarousel/Slider.js
@@ -10,23 +10,25 @@ function Slider() {
             <>
             <div className="container">
             <Carousel >
-                <div >
+                <div className="felxi" >
+                    <div className="abso">
                     <img classname="abs"  style={{height:"437px",width:"530px"}} src="" />
-                    
+                    </div>
+                    <div className="about">
+            <h1 className="head">About Tantra Fiesta</h1>
+            <p className="para">Tantra Fiesta is the Annual Technical Fest of Indian Institute of Nagpur. With Tantra comes learning, exploration blah blah. If you are a student looking for blah this is the right place for you. Tantra Fiesta is the Annual Technical Fest of Indian Institute of Nagpur. With Tantra comes learning, exploration blah blah. If you are a student looking for blah this is the right place for you. Tantra Fiesta is the Annual Technical Fest of Indian Institute of Nagpur. With Tantra comes learning, exploration blah blah. If you are a student looking for blah this is the right place for you. Tantra Fiesta is the Annual Technical Fest of Indian Institute of Nagpur. With Tantra comes learning, exploration blah blah. If you are a student looking for blah this is the right place for you. </p>
+            </div>
                 </div>
-                <div >
+                <div  className="abso">
                     <img classname="abs" src=""  />
                    
                 </div>
-                <div>
+                <div className="abso">
                     <img classname="abs" src="assets/3.jpeg" />
                    
                 </div>
             </Carousel>
-            <div className="about">
-            <h1 className="head">About Tantra Fiesta</h1>
-            <p className="para">Tantra Fiesta is the Annual Technical Fest of Indian Institute of Nagpur. With Tantra comes learning, exploration blah blah. If you are a student looking for blah this is the right place for you. Tantra Fiesta is the Annual Technical Fest of Indian Institute of Nagpur. With Tantra comes learning, exploration blah blah. If you are a student looking for blah this is the right place for you. Tantra Fiesta is the Annual Technical Fest of Indian Institute of Nagpur. With Tantra comes learning, exploration blah blah. If you are a student looking for blah this is the right place for you. Tantra Fiesta is the Annual Technical Fest of Indian Institute of Nagpur. With Tantra comes learning, exploration blah blah. If you are a student looking for blah this is the right place for you. </p>
-            </div>
+          
             </div>
             </>
         );
