2f2b188d8a59c754d7a15e7d1bbc4ed14162c81f
Harsh Gadage <hgadge1605@gmail.com>
2021-10-06 11:23:46 +0530

    Done some css changes in competition part

--- src/Components/competitions/competitions.js
+++ src/Components/competitions/competitions.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Competitions.css";
-import Cards from "../cards/Cards.js"
+import Cards from "../cards/Cards.js";
 
 const Competitions = () => {
   return (
@@ -9,23 +9,13 @@ const Competitions = () => {
         <h1>Competitions</h1>
         <div className="view">
           <span>View All</span>
-          <img src="/Assets/images/Arrow1.svg"
-          alt="Arrow" />
+          <img src="/Assets/images/Arrow1.svg" alt="Arrow" />
         </div>
       </div>
       <div class="card">
-      <Cards 
-      img="Assets/images/Asset41.svg"
-      eventName="Hackathon"
-      />
-      <Cards 
-      img="Assets/images/Asset11.svg"
-      eventName="Hackathon"
-      />
-      <Cards 
-      img="Assets/images/Asset51.svg"
-      eventName="Hackathon"
-      />
+        <Cards img="Assets/images/Asset41.svg" eventName="Hackathon" />
+        <Cards img="Assets/images/Asset11.svg" eventName="Design-a-thon" />
+        <Cards img="Assets/images/Asset51.svg" eventName="Coding Contest" />
       </div>
     </div>
   );
