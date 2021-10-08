afe60fe448e6092e4a741792b887f1b564b408cc
Bhavesh <bhic2030@gmail.com>
2021-10-08 18:08:48 +0530

    stabilize master and readme linkedin update

--- src/Pages/home/Home.js
+++ src/Pages/home/Home.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import "./Home.css"
 import Landing from "./Components/Landing/landing";
-import Competitions from "../../Components/competitions/competitions"
+// import Competitions from "../../Components/competitions/competitions"
 
 const Home = () => {
     return (
         <div className="home-container" >
             <Landing></Landing>
-            <Competitions></Competitions>
+            {/* <Competitions></Competitions> */}
         </div>
     )
 }
