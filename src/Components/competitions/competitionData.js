07dc12ed17bd81fd18a30416c113c24139c08f98
Bhavesh <bhic2030@gmail.com>
2021-10-08 16:26:27 +0530

    competitions section complete - home page

---
+++ src/Components/competitions/competitionData.js
@@ -0,0 +1,26 @@
+// importing SVGs
+import hackathonSvg from "./assets/hackathon.png";
+import designathonSvg from "./assets/designathon.png";
+import codingContestSvg from "./assets/codingcontest.png";
+
+// Testing data
+export default [
+  {
+    id: 1,
+    competitionImage: hackathonSvg,
+    competitionTitle: "Hackathon",
+    competitionDeadline: "15th Oct 11:59pm IST",
+  },
+  {
+    id: 2,
+    competitionImage: designathonSvg,
+    competitionTitle: "Design-a-thon",
+    competitionDeadline: "15th Oct 11:59pm IST",
+  },
+  {
+    id: 3,
+    competitionImage: codingContestSvg,
+    competitionTitle: "Coding Contest",
+    competitionDeadline: "15th Oct 11:59pm IST",
+  },
+];
--- src/Components/competitions/competitions.js
+++ src/Components/competitions/competitions.js
@@ -1,22 +1,30 @@
 import React from "react";
-import "./Competitions.css";
-import Cards from "../cards/Cards.js";
+import "./competitions.css";
+import arrow from "./assets/Arrow1.svg";
+import Card from "./card/card";
+
+// import competitionData
+import competitionData from "./competitionData";
+
+import CarouselComponent from "../carousel/Carousel";
 
 const Competitions = () => {
   return (
     <div className="competition-container">
       <div className="header">
         <h1>Competitions</h1>
-        <div className="view">
-          <span>View All</span>
-          <img src="/Assets/images/Arrow1.svg" alt="Arrow" />
-        </div>
+        <a href="">
+          <span className="view">
+            <span>View All</span>
+            <img src={arrow} alt="Arrow" />
+          </span>
+        </a>
       </div>
-      <div class="card">
-        <Cards img="Assets/images/Asset41.svg" eventName="Hackathon" />
-        <Cards img="Assets/images/Asset11.svg" eventName="Design-a-thon" />
-        <Cards img="Assets/images/Asset51.svg" eventName="Coding Contest" />
-      </div>
+      <CarouselComponent>
+        {competitionData.map((competition) => {
+          return <Card key={competition.id} {...competition}></Card>;
+        })}
+      </CarouselComponent>
     </div>
   );
 };
