fa3fe740d1446eb9c3fed3438d4aaa320e1bf6f6
Bhavesh <bhic2030@gmail.com>
2021-10-08 15:40:51 +0530

    carousel component complete

--- src/Pages/home/Home.js
+++ src/Pages/home/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import "./Home.css"
 import Landing from "./Components/Landing/landing";
+import Competitions from "../../Components/competitions/competitions"
 
 const Home = () => {
     return (
-        <div>
+        <div className="home-container" >
             <Landing></Landing>
+            <Competitions></Competitions>
         </div>
     )
 }
