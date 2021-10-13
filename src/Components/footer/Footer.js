11f93f1836d990f3fd82014c033f3449bdb03ba1
Ritu Bhangale <bt20cse021@iiitn.ac.in>
2021-10-13 21:29:00 +0530

    Footer populated
    Footer

--- src/Components/footer/Footer.js
+++ src/Components/footer/Footer.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { ReactComponent as Arrow } from "./assets/arrow.svg";
-import { ReactComponent as Square } from "./assets/square.svg";
-import { ReactComponent as FacebookIcon } from "./assets/facebook.svg";
-import { ReactComponent as InstagramIcon } from "./assets/instagram.svg";
-import { ReactComponent as LinkedInIcon } from "./assets/linkedin.svg";
 import "./Footer.css";
 import { Link } from "react-router-dom";
+import { ReactComponent as Arrow } from "./assets/arrow.svg";
+import { ReactComponent as Square } from "./assets/square.svg";
+import { ReactComponent as FacebookIcon } from "./assets/social/facebook.svg";
+import { ReactComponent as InstagramIcon } from "./assets/social/instagram.svg";
+import { ReactComponent as LinkedInIcon } from "./assets/social/linkedin.svg";
+import { ReactComponent as DiscordIcon} from "./assets/social/discord.svg"
+import { ReactComponent as YoutubeIcon} from "./assets/social/youtube.svg"
+import SkillLync from "./assets/sponsors/skillync-icon.png"
+import Dtoc from "./assets/sponsors/d2c-icon.png"
+import Mentro from "./assets/sponsors/mentro-icon.png"
+import Gfg from "./assets/sponsors/gfg-icon.png"
+import MadeEasy from "./assets/sponsors/madeasy-icon.png"
 
 const Footer = () => {
   return (
@@ -44,6 +51,12 @@ const Footer = () => {
               >
                 <LinkedInIcon></LinkedInIcon>
               </a>
+              <a href="https://discord.gg/4zWEYA5VZv" target="_blank">
+                <DiscordIcon></DiscordIcon>
+              </a>
+              <a href="https://www.youtube.com/channel/UC7xty40bNXvYPw_nrOQPB1Q/featured" target="_blank">
+                <YoutubeIcon></YoutubeIcon>
+              </a>
             </div>
             <p>Connect with us here</p>
           </div>
@@ -52,21 +65,16 @@ const Footer = () => {
           <div className="main-sponsors">
             <h2>Our Sponsors</h2>
             <div className="square">
-              <Square></Square>
-              <Square></Square>
-              <Square></Square>
-              <Square></Square>
-              <Square></Square>
+              <a href="https://skill-lync.com/"  target="_blank"><img src={SkillLync} alt="SkillLync" className="skillLync"/></a>
+              <a href="https://dare2compete.com/" target="_blank"><img src={Dtoc} alt="D2C" /></a>
             </div>
           </div>
           <div className="co-sponsors">
             <h2>Our Co-Sponsors</h2>
             <div className="square">
-              <Square></Square>
-              <Square></Square>
-              <Square></Square>
-              <Square></Square>
-              <Square></Square>
+              <a href="https://www.geeksforgeeks.org/" target="_blank"><img src={Gfg} alt="GFG" /></a>
+              <a href="https://mentro.tech/" target="_blank"><img src={Mentro} alt="Mentro" /></a>
+              <a href="https://www.madeeasy.in/" target="_blank"><img src={MadeEasy} alt="MadeEasy" /></a>
             </div>
           </div>
         </div>
